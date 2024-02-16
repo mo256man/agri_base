403ba5f542ffdfaf6c458926461d9c9dbebcc859
mo256man <48610559+mo256man@users.noreply.github.com>
2024-02-16 14:12:44 +0900

    Add files via upload

--- autoexec.sh
+++ autoexec.sh
@@ -1,4 +1,9 @@
-echo "start autoexec"
-DISPLAY=:0
-bash run_app.sh
+# contec start
+cd /home/tab/contec/cdio/config
+./contec_dio_start.sh
 
+# agribecky start
+PYTHONPATH=/user/local/lib
+DISPLAY=:0
+cd /home/tab/agri/
+python app.py
